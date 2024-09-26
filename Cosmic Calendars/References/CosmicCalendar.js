e799fe78e7f69049914d0bd7b405bf852fd44def
KaliBasenji42 <jacobdelzer369@gmail.com>
2024-09-26 13:10:07 -0600

    Hello!

---
+++ Cosmic Calendars/References/CosmicCalendar.js
@@ -0,0 +1,45 @@
+// Set
+
+stat.style.backgroundColor = "rgb(128, 255, 128)";
+stat.innerHTML = "Awaiting Input";
+
+render();
+
+// Variables and Constants
+
+let timeA = new Date();
+let timeB = new Date();
+
+// Functions
+
+function setFormDate(id, date) {
+  
+  let formInp = document.getElementById(id)
+  
+  formInp.value = date;
+  
+}
+
+// Objects
+
+
+
+// Inp Form
+
+document.getElementById("valFormA").addEventListener("submit", function (event) {
+  
+  event.preventDefault();
+  
+  // Values
+  
+  let timeInp = document.getElementById("timeA").value;
+  
+  let time = new Date(timeInp);
+  setFormDate('timeA', time);
+  
+  console.log(time.getTimezoneOffset());
+  
+  stat.style.backgroundColor = "rgb(128, 255, 128)";
+  stat.innerHTML = "Calculated";
+  
+});
