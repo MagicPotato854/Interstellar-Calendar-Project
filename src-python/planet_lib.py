488d5741fcc97b572a3d87a17032b05008df2224
MagicPotato854 <masonwkohler@gmail.com>
2024-10-01 15:01:11 -0600

    small changes to planet_lib.py

--- src-python/planet_lib.py
+++ src-python/planet_lib.py
@@ -32,17 +32,21 @@ def hours_to_mars(hours):
     return year + 1, int(day + 1), int(hours), minutes, round(seconds, 3)
 
 
-def print_planet_time(date):
+def print_planet_time(date, day_len=0):
     """
     Prints a date in human readable format.
     date should be a tuple formatted like the following:
     (year, day, hours, minutes, seconds)
     """
-    print(f"Year {date[0]}, day {date[1]}, " + "{:02}:{:02}:{:02}".format(date[2], date[3], floor(date[4])))
-
+    if day_len < 100:
+        print(f"Year {date[0]}, day {date[1]}, " + "{:02}:{:02}:{:02}".format(date[2], date[3], floor(date[4])))
+    elif day_len >= 1000:
+        print(f"Year {date[0]}, day {date[1]}, " + "{:04}:{:02}:{:02}".format(date[2], date[3], floor(date[4])))
+    else:
+        print(f"Year {date[0]}, day {date[1]}, " + "{:03}:{:02}:{:02}".format(date[2], date[3], floor(date[4])))
 
 spectime = 17739736.883
 date = hours_to_earth(spectime)
 print_earth_time(date)
 print(hours_to_mars(spectime))
-print_planet_time(hours_to_mars(spectime))
+print_planet_time(hours_to_mars(spectime), 11)
